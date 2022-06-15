80597b44195cf6e2a04e5bbdf33390617b435076
chuahziyang <czyang2002@gmail.com>
2022-06-16 03:34:47 +0800

    Initialize login

---
+++ components/FormField.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { Field, ErrorMessage } from "vee-validate";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  rules: {
+    type: Function,
+    default: null,
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+});
+
+const { placeholder, rules, label, name, type } = props;
+
+console.log({ placeholder, rules, label, name, type });
+console.log("asd");
+
+function returnfalse() {
+  return false;
+}
+</script>
+
+<template>
+  <Field
+    :name="name"
+    :label="label"
+    :type="type"
+    :placeholder="placeholder"
+    :rules="rules"
+    :change="change"
+  />
+  <ErrorMessage :name="name" />
+</template>
